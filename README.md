a70491792a62875b02c07d2d0a4e09f666a39b61
lswn92 <ujunja@naver.com>
2022-09-28 23:14:03 +0900

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# serverless-lambda-dynamo-api
+# serverless-lambda-dynamo-apiは？
+
+このプロジェクトは、SERVERLESS FRAMEWORKを使用してAPI Gateway - LambdaからDynamoDBにデータアプロード、変更、削除するAPIをデプロイするプロジェクトです。
+
+
+# 使用方法
+### node_modulesインストール
+
+```
+npm install
+```
+
+### デプロイ
+```
+serverless deploy
+sls deploy
+```
+
---
+++ handler/createTodo.js
@@ -0,0 +1,46 @@
+const AWS = require("aws-sdk");
+
+/** serverless.yml environment 参照 */
+const LSWN_TABLE = process.env.LSWN_TABLE;
+const dynamoDbClient = new AWS.DynamoDB.DocumentClient();
+const uuid = require("uuid");
+
+/** serverless.yml fuctionsのhandler 参照 　*/
+exports.createTodo = (event, context, callback) => {
+
+    /** 
+     * new Date()はあまりお勧めしません。下記のリンクを参考してください。
+     * https://www.youtube.com/watch?v=CSWc0HYjxEs */
+    const timestamp = new Date().getTime();
+    const data = JSON.parse(event.body);
+    if (typeof data.todo !== "string") {
+        console.log("validation Failed");
+        return;
+    }
+    /** time based uuid 生成 */
+    const keyId = uuid.v1();
+    const params = {
+        TableName: LSWN_TABLE,
+        Item: {
+            userId: keyId,
+            todo: data.todo,
+            checked: false,
+            createdAt: timestamp,
+            updatedAt: timestamp
+        }
+    }
+
+    // data = params
+    dynamoDbClient.put(params, function(error, data) {
+        if (error) {
+            console.error(error);
+            callback(new Error(error));
+            return;
+        }
+        const response = {
+            statusCode: 200,
+            body: JSON.stringify(data.Item),
+        }
+        callback(null, response);
+    });
+}
